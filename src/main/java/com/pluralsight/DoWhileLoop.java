b5778e7faba6d2bc32400a5630f96b366affd84e
mariannaaspuria <aspuria.mp@gmail.com>
2025-10-02 15:34:04 -0500

    do while loop commit

---
+++ src/main/java/com/pluralsight/DoWhileLoop.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+public class DoWhileLoop {
+
+    public static void main(String[] args) {
+        int count = 1;
+
+        do {
+            System.out.println("I love java");
+            count++;
+        } while (count <= 5);
+    }
+
+}
